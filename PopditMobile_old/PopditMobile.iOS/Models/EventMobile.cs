b14926cf270c4b5e399158e8ed1b43ef47854208
Justin Stimson <justin@justinstimson.com>
2017-06-25 14:32:57 -0400

    Added PopditiOS project with mobile app.

---
+++ PopditMobile_old/PopditMobile.iOS/Models/EventMobile.cs
@@ -0,0 +1,17 @@
+﻿namespace PopditMobile.iOS
+{
+    using System;
+
+    public partial class EventMobile
+    {
+        public int Id { get; set; }
+        //public int ProfileId { get; set; }
+        public int BubbleId { get; set; }
+        public string TimestampJson
+        {
+            get { return String.Format("{0:yyyy-MM-dd HH:mm:ss}", Timestamp); }
+            set { Timestamp = DateTime.Parse(value); }
+        }
+        private DateTime Timestamp { get; set; }
+    }
+}
