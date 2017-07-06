1ec0788a7c922eb4d3557f446efe79647c867ba8
Justin Stimson <justin@justinstimson.com>
2017-07-06 17:44:43 -0400

    Began work on mobile bubble catalog refreshing.

--- PopditWeb/Controllers/Controller.cs
+++ PopditWeb/Controllers/Controller.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Newtonsoft.Json.Linq;
-using System.Net;
-using System.Text;
 using System.IO;
-using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Net.Http.Headers;
--- PopditiOS/LocationManager.cs
+++ PopditiOS/LocationManager.cs
@@ -3,15 +3,23 @@ using PopditiOS.Models;
 using CoreLocation;
 using UIKit;
 using Foundation;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
 
 namespace PopditiOS
 {
     public class LocationManager
     {
         protected CLLocationManager LocMgr;
-        string SecToken = "";
-        Zone Zone = new Zone();
-        List<BubbleMobile> BubbleCatalog = new List<BubbleMobile>();
+        string SecToken = "8126502080";
+        const int bubbleZoneRadius = 3000; // Radius in meters.  TBD - move to config
+        const int refreshZoneRadius = 2000; // Radius in meters.  TBD - move to config
+        List<BubbleMobile> BubbleCatalog;
 
         public event System.EventHandler<BubblePoppedEventArgs> BubblePopped = delegate { };
 
@@ -26,6 +34,7 @@ namespace PopditiOS
 
         // Get the security token for this mobile device.  Try this object, then the cookie, then the server.
         // If this function fails, the user needs to sign in.
+        // TBD - Add error handling to force sign in.
         string SecurityToken
         {
           get
@@ -102,16 +111,72 @@ namespace PopditiOS
 
         // A bubble was popped.  Notify the server and display the notification string to the user.
         void Pop()
-        { }
-
-        // The user moved.  Get a new catalog of bubbles from the server.
-        void RefreshBubbleCatalog()
         {
-            // Make sure the user's credentials are available.
-            string token = SecurityToken;
-            // Get the user's location.
+            // If the user left the refresh zone, then refresh the bubble catalog.
+            // Otherwise, if a location bubble was popped, notify the server and display a notification message.
+        }
+
+        // The user left the refresh zone.  Get a new catalog of bubbles from the server.
+        async void RefreshBubbleCatalog()
+        {
+            // Get the user's location and code it as the bubble zone.
+            CLLocation location = LocMgr.Location;
+            Zone bubbleZone = new Zone();
+            bubbleZone.Latitude = (decimal)location.Coordinate.Latitude; // TBD double
+            bubbleZone.Longitude = (decimal)location.Coordinate.Longitude; // TBD double
+            bubbleZone.Radius = bubbleZoneRadius;
             // Define a zone and set and event handler for it.
             // Get all the bubbles in the zone and set event handlers for them.
+            Stream json = await WebApiPost("api/Bubble", bubbleZone);
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<BubbleMobile>));
+            BubbleCatalog = (List<BubbleMobile>)serializer.ReadObject(json);
+            // Create a new bubble to represent the new refresh zone and add it to the bubble catalog.
+            // When the user pops it from inside, we will refresh again.
+        }
+
+        protected async Task<Stream> WebApiPost(string servicePath, Object content)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:83/"); // TBD - move to config
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Add("Authorization", SecurityToken);                    
+                    string json = JsonConvert.SerializeObject(content);
+                    StringContent sc = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(servicePath, sc).ConfigureAwait(false);
+                    return response.Content.ReadAsStreamAsync().Result;
+                }
+            }
+            catch (Exception e)
+            {
+                // TBD - Handle error
+                return null;
+            }
+        }
+
+        protected async Task<Stream> WebApiGet(string servicePath)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://localhost:83/"); // TBD - make configurable
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Add("Authorization", SecurityToken);  // TBD - this is just a hack for testing
+
+                    HttpResponseMessage response = await client.GetAsync(servicePath).ConfigureAwait(false);
+                    return response.Content.ReadAsStreamAsync().Result;
+                }
+            }
+            catch (Exception e)
+            {
+                // TBD - Handle error
+                return null;
+            }
         }
 
         public void StartLocationUpdates() { RefreshBubbleCatalog(); } // Kick off the first refresh.
